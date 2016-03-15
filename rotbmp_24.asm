40f8d60dced495ba6df09038f0e02733593d4168
Igor <igor.klemenski@gmail.com>
2016-03-15 21:26:34 +0100

    fixed comment formatting and added sample pictures

--- rotbmp_24.asm
+++ rotbmp_24.asm
@@ -1,55 +1,56 @@
-global rotbmp24
+global _rotbmp24
 
 ;define parameters' addresses
 %define     img         [ebp+8]     ;pointer to bmp image   
 %define     tmp         [ebp+12]    ;destination buffer
-%define 	width 		[ebp+16]   
+%define     width 		[ebp+16]   
 
 
-rotbmp24:
+_rotbmp24:
 	;making stack frame
 	push    ebp
 	mov     ebp, esp
-	push 	esi 				   ;idx_src
-	push 	edi 				   ;idx_dest
+	push 	esi                    ;idx_src
+	push 	edi                    ;idx_dest
 	push 	ebx
 
-	;stride=3*width + padding	   ;distance between pixels in the same column
+	;stride=3*width + padding      ;distance between pixels in the same column
 	mov     ebx, width         
 	lea 	ebx, [ebx + ebx*2]  
-	mov 	eax, ebx 			   ;save val for padding computation     
+	mov 	eax, ebx               ;save val for padding computation     
 	lea 	ebx, [ebx + 3]
-	and     ebx, 0xfffffffc 	   ;store stride in $EBX
+	and     ebx, 0xfffffffc        ;store stride in $EBX
 
 	;get padding size
 	mov 	edx, ebx
 	sub		edx, eax			   
-	bswap 	ecx					   ;trick to use  the upper half of register
-	mov 	cl, dl				   ;store padding in upper part of ecx
+	bswap 	ecx                    ;trick to use  the upper half of register
+	mov 	cl, dl                 ;store padding in upper part of ecx
 	bswap 	ecx
 
-	;idx_dest = (3*width + padding)*(width-1)	  ;start pasting pixels in the bottom-left corner of the destination buffer (tmp @ $EDI)
+	;idx_dest = (3*width + padding)*(width-1)	  
+	;start pasting pixels in the bottom-left corner of the destination buffer (tmp @ $EDI)
 	mov 	eax, width 			   
 	dec 	eax
-	mul 	ebx 			       ; @line_22
+	mul 	ebx                    ; @line_22
 	mov 	edi, eax
 	add 	edi, tmp
 
 	;load img ptr
 	mov 	esi, img
 
-i_beg:							   ;enter outer loop
+i_beg:                             ;enter outer loop
 	mov 	dx, width
 
-j_beg:							   ;enter inner loop
+j_beg:                             ;enter inner loop
 	mov 	cx, width
 
 j_in:
 	;actual copying
-	mov 	eax, [esi]			   ;copy  4 bytes
-	mov 	[edi], ax			   ;paste 2 bytes to destination buffer 
-	shr 	eax, 16 			   ;get   rid of the right half of eax and get access to its left half by shifting
-	mov 	[edi + 2], al 		   ;paste the 1  remaining byte of pixel   (3  in total, 24bpp)
+	mov 	eax, [esi]             ;copy  4 bytes
+	mov 	[edi], ax              ;paste 2 bytes to destination buffer 
+	shr 	eax, 16                ;get   rid of the right half of eax and get access to its left half by shifting
+	mov 	[edi + 2], al          ;paste the 1  remaining byte of pixel   (3  in total, 24bpp)
 
 	;adjust idx_dest after j-change
 	sub 	edi, ebx
